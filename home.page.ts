a683e0e43a24688584feb3c36b07ccfd6b272d58
lcrodrigues7 <lucasrodriguespinto4321@gmail.com>
2025-09-07 20:36:25 -0300

    formadora1
    formadora

---
+++ home.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterModule], // IMPORTANTE
+})
+export class HomePage {}
