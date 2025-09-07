466bdec6b09c281d7601a6a156d68b8b880de3cf
lcrodrigues7 <lucasrodriguespinto4321@gmail.com>
2025-09-07 20:38:32 -0300

    formadora1
    formadora

---
+++ sobre.page.spec.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.page.html',
+  styleUrls: ['./sobre.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterModule], // IMPORTANTE
+})
+export class SobrePage {}
